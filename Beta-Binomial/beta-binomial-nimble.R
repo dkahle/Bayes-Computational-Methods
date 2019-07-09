4e7df6d56a67e5942e72f196b9e3aefc1890756b
evanmiyakawa <evanmiyakawa@gmail.com>
2019-07-09 11:12:42 -0500

    fixing

--- Beta-Binomial/beta-binomial-nimble.R
+++ Beta-Binomial/beta-binomial-nimble.R
@@ -10,4 +10,4 @@ inits <- list(theta = 0.5)
 
 results <- nimbleMCMC(rCode, data = data, inits = inits, monitors = c("theta"), 
                       nchains = 4, niter = 11000, nburnin = 1000, summary = TRUE)
-results$summary
+results$summary$all.chains
