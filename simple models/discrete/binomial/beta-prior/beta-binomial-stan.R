33f8480eebecb4b22ed4f07d66f77c2337d79f44
evanmiyakawa <evanmiyakawa@gmail.com>
2019-07-19 10:40:33 -0500

    stan path fixed

--- simple models/discrete/binomial/beta-prior/beta-binomial-stan.R
+++ simple models/discrete/binomial/beta-prior/beta-binomial-stan.R
@@ -3,10 +3,10 @@ library(here)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
-stan_file <- here("beta-binomial", "beta-binomial.stan")
+stan_file <- here("simple models", "discrete", "binomial", "beta-prior", "beta-binomial.stan")
 
 data <- list(y=2)
 
-fit <- stan(file = 'beta-binomial.stan', data = data, 
+fit <- stan(file = stan_file, data = data, 
             iter = 1000, chains = 4)
 print(fit)
