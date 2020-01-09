92cc3f66ee6bfd47a1492bb159e98c6ec4149bb1
evanmiyakawa <evanmiyakawa@gmail.com>
2020-01-09 16:13:25 -0600

    8 schools update

--- 8-Schools/8-schools-greta.R
+++ 8-Schools/8-schools-greta.R
@@ -1,12 +1,13 @@
 ## load required packages and set basic options
 ################################################################################
 
-library("here")
 library("tidyverse"); theme_set(theme_minimal())
 library("parallel"); options(mc.cores = detectCores())
-library("rstan"); rstan_options(auto_write = TRUE)
-library("bench")
+library("greta")
 library("bayesplot")
+library("bench")
+library("here")
+
 
 ## generate/specify data
 ################################################################################
@@ -15,65 +16,53 @@ J <- 8
 y <- c(28,  8, -3,  7, -1,  1, 18, 12)
 sigma <- c(15, 10, 16, 11,  9, 11, 10, 18)
 
-stan_data <- list(
-  "J" = J,
-  "y" = y, 
-  "sigma" = sigma
-)
+y <- as_data(y)
+sigma <- as_data(sigma)
 
 
-## specify stan model
+## specify greta model
 ################################################################################
 
-# read it in from file
-stan_file <- here("8-schools", "8-schools.stan")
+tau <- uniform(0,1000)
+mu <- normal(0,100)
+eta <- normal(0,1, dim = J)
+theta <- mu + tau * eta
 
-# file.show(stan_file)
+distribution(sigma) <- uniform(0,1000)
+distribution(y) <- normal(theta, sigma)
 
+greta_model <- model(mu, tau, eta, theta)
 
+plot(greta_model)
 
 ## configure model settings
 ################################################################################
 
-n_chains <- 4L
+n_chains <- 4
 n_iter <- 1e4L
 n_warmup <- 1e3L
 
 
+
 ## fit model
 ################################################################################
+
 source(here("currently-benchmarking.R"))
 
 if (!currently_benchmarking()) {
-  stan_fit <- stan(
-    "file" = stan_file, "data" = stan_data, 
-    "chains" = n_chains, "iter" = n_iter, "warmup" = n_warmup, 
-    "control" = list("adapt_delta" = 0.99)
-  )
   
+  
+  greta_fit <- mcmc(
+    "model" = greta_model, "n_samples" = n_iter,
+    "warmup" = n_warmup, "chains" = n_chains
+  )
   
   
   
   ## assess fit
   ################################################################################
   
-  summary(stan_fit)$summary
-  get_posterior_mean(stan_fit)
-  stan_dens(stan_fit) + theme_bw()
-  stan_fit %>% as.array() %>% bayesplot::mcmc_dens()
-  
-  
-  
-  ## assess convergence issues 
-  ###################################################################################
-  
-  stan_fit %>% as.array() %>% mcmc_acf_bar()
-  stan_fit %>% as.array() %>% mcmc_pairs()
-  stan_fit %>% as.array() %>% mcmc_trace()
-  
-  # see each chain
-  stan_fit %>% rstan::extract(permuted = FALSE, inc_warmup = TRUE)
+  summary(greta_fit)
   
 }
-
 
