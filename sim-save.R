fc0049546e51b619bf39f2b0e3c85a96817153a4
evanmiyakawa <evanmiyakawa@gmail.com>
2020-02-19 09:32:01 -0600

    sim updates

--- sim-save.R
+++ sim-save.R
@@ -1,6 +1,6 @@
 x <- run_benchmark(rds_file_location)
-linear_regression_results <- x
-saveRDS(linear_regression_results, file = "linear-regression-results.rds")
+logistic_regression_results <- x
+saveRDS(logistic_regression_results, file = "logistic-regression-results.rds")
 x
 
 # readRDS("exponential-gamma-results.rds")
