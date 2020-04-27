5a9cadaabc61cc6d4ecd68261bc8f82d392fc864
evanmiyakawa <evanmiyakawa@gmail.com>
2020-04-27 12:14:05 -0500

    ppp update and adding accuracy bench

---
+++ benchmark-accuracy-function.R
@@ -0,0 +1,67 @@
+library(distr)
+library(distrEx)
+
+# true_dist <- distr::Beta(3,9)
+# 
+# num_iterations <- 3
+
+run_accuracy_benchmark <- function(true_dist, num_iterations, n_iter = 1e4L, n_warmup = 1e3L) {
+  one_iteration <- function(iter) {
+    jags_fit <- run.jags(
+      "model" = jags_model, "data" = jags_data, "monitor" = jags_monitor,
+      "n.chains" = n_chains, "sample" = n_iter, "burnin" = n_warmup
+    )
+    nimble_fit <- nimbleMCMC(
+      "code" = nimble_model, "constants" = nimble_constants, "data" = nimble_data,
+      "inits" = nimble_inits, "monitors" = nimble_monitor, "nchains" = n_chains,
+      "niter" = n_iter, "nburnin" = n_warmup, "summary" = TRUE
+    )
+    stan_fit <- stan(
+      "file" = stan_file, "data" = stan_data,
+      "chains" = n_chains, "iter" = n_iter, "warmup" = n_warmup,
+      "control" = list("adapt_delta" = 0.99)
+    )
+    greta_fit <- mcmc(
+      "model" = greta_model, "n_samples" = n_iter,
+      "warmup" = n_warmup, "chains" = n_chains
+    )
+    
+    jags_fit_object <- jags_fit$mcmc %>% as.array()
+    # dim(jags_fit_object) <- c(dim(jags_fit_object), 1)
+    jags_fit_condense <- c(jags_fit_object[,1], 
+                           jags_fit_object[,2],
+                           jags_fit_object[,3],
+                           jags_fit_object[,4])
+    stan_fit_object <- stan_fit %>% as.array()
+    stan_fit_condense <- c(
+      stan_fit_object[,1,1],
+      stan_fit_object[,2,1],
+      stan_fit_object[,3,1],
+      stan_fit_object[,4,1]
+    )
+    greta_fit_object <- greta_fit %>% as.array()
+    greta_fit_condense <- c(greta_fit_object[,1], 
+                           greta_fit_object[,2],
+                           greta_fit_object[,3],
+                           greta_fit_object[,4])
+    
+    jags_dist <- TotalVarDist(true_dist,jags_fit_condense)
+    stan_dist <- TotalVarDist(true_dist,stan_fit_condense)
+    greta_dist <- TotalVarDist(true_dist,greta_fit_condense)
+    
+    tibble(
+      iter = iter,
+      jags_dist = jags_dist,
+      stan_dist = stan_dist,
+      greta_dist = greta_dist
+    )
+  }
+  
+  dist_results <- 1:num_iterations %>% 
+    map_dfr(one_iteration)
+ 
+  skimr::skim(dist_results[-1])
+  
+  
+  
+}
--- simple-models/discrete/binomial/beta-prior/beta-binomial-benchmark.R
+++ simple-models/discrete/binomial/beta-prior/beta-binomial-benchmark.R
@@ -7,8 +7,8 @@ library("bench")
 library("here")
 library("rjags"); library("runjags")
 library("R2OpenBUGS")
-library("nimble")
 library("rstan"); rstan_options(auto_write = FALSE)
+library("nimble")
 options("bayes_benchmark" = TRUE)
 
 ## Source code
@@ -18,7 +18,7 @@ source(here("simple-models", "discrete", "binomial", "beta-prior", "beta-binomia
 source(here("simple-models", "discrete", "binomial", "beta-prior", "beta-binomial-bugs.R"))
 source(here("simple-models", "discrete", "binomial", "beta-prior", "beta-binomial-nimble.R"))
 source(here("simple-models", "discrete", "binomial", "beta-prior", "beta-binomial-stan.R"))
-# source(here("simple-models", "discrete", "binomial", "beta-prior", "beta-binomial-greta.R"))
+source(here("simple-models", "discrete", "binomial", "beta-prior", "beta-binomial-greta.R"))
 
 options("bayes_benchmark" = FALSE)
 
@@ -28,7 +28,7 @@ n_chains <- 4L
 n_iter <- 1e4L
 n_warmup <- 1e3L
 
-## benchmarking
+## benchmarking computation time
 ###################################################################################
 
 num_iterations <- 1 # Number of times you want to benchmark each model\
@@ -41,6 +41,15 @@ source(here("benchmark-function.R"))
 run_benchmark(rds_file_location)
 # run_benchmark(rds_file_location, stan_compile = TRUE)
 
+## benchmarking accuracy
+###################################################################################
 
+num_iterations <- 10 # Number of times you want to benchmark each model\
+
+source(here("benchmark-accuracy-function.R"))
+
+true_dist <- distr::Beta(3,9)
+
+run_accuracy_benchmark(true_dist,num_iterations, n_iter, n_warmup)
 
 
